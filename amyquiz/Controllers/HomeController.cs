d5f59d15c93b1f416cface07560a677fe5fe3aeb
Lauren Holloway <laurenholloway1010@gmail.com>
2015-11-21 09:30:34 -0500

    setting up

---
+++ amyquiz/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace amyquiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Quiz()
+        {
+            return View();
+        }
+        public ActionResult Results()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
